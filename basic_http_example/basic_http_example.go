ba72bcf0be8330493c4092e9330c25ec9faf70da
KuoYu-chiung <genekuo@cybersoft4u.com>
2017-09-19 11:44:47 +0800

    Hello world

---
+++ basic_http_example/basic_http_example.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := 8080
+
+	http.HandleFunc("/helloworld", helloworldHandler)
+
+	log.Printf("Server starting on port %v\n", 8080)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+
+}
+
+func helloworldHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World\n")
+}
